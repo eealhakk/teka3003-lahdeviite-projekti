ca79c2e896fbef90c6d98cdcdf364f0d2dcba960
Eeli Häkkinen <eeli.hakkinen00@gmail.com>
2025-11-21 16:48:39 +0200

    lisätty entities

--- pohjaa.py
+++ pohjaa.py
@@ -1,40 +1,3 @@
-data = """@inproceedings{VPL11,
-            author = {Vihavainen, Arto and Paksula, Matti and Luukkainen, Matti},
-            title = {Extreme Apprenticeship Method in Teaching Programming for Beginners.},
-            year = {2011},
-            booktitle = {SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education},
-        }
-        
-        @article{CBH91,
-            author = {Allan Collins and John Seely Brown and Ann Holum},
-            title = {Cognitive apprenticeship: making thinking visible},
-            journal = {American Educator},
-            year = {1991},
-            volume = {6},
-            pages = {38--46}
-        }
-        
-        @book{Martin09,
-            author = {Martin, Robert},
-            title = {Clean Code: A Handbook of Agile Software Craftsmanship},
-            year = {2008},
-            publisher = {Prentice Hall},
-        }
-        """
-
-
-keywords = ["VPL11", "TST2023"]
-authors = ["kissa", "koira"]
-titles = ["title1", "title2"]
-yers = [2012, 2015, 2025]
-booktitles = []
-
-class Reference:
-    def __init__(self, type, place):
-        self.type = type
-        self.place = place
-
-
 class Inproceeding:
     def __init__(self, key, author = "", title = "", year = 0, booktitle = ""):
         self.key = key
@@ -62,3 +25,56 @@ class Book:
         self.title = title
         self.year = year
         self.publisher = publisher
+
+
+class Reference:
+    def __init__(self, type = []):
+        self.type = [type]
+
+    def add_reference():
+        return
+    
+        
+    
+
+
+
+##==========================================================================================
+##Data
+
+VPL11 = Inproceeding(
+    key="VPL11",
+    author="Vihavainen, Arto and Paksula, Matti and Luukkainen, Matti",
+    title="Extreme Apprenticeship Method in Teaching Programming for Beginners.",
+    year=2011,
+    booktitle="SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education"
+)
+
+CBH91 = Article(
+    key="CBH91",
+    author="Allan Collins and John Seely Brown and Ann Holum",
+    title="Cognitive apprenticeship: making thinking visible",
+    journal="American Educator",
+    year=1991,
+    volume=6,
+    pages="38--46"
+)
+
+Martin09 = Book(
+    key="Martin09",
+    author="Martin, Robert",
+    title="Clean Code: A Handbook of Agile Software Craftsmanship",
+    year=2008,
+    publisher="Prentice Hall"
+)
+
+entries = [VPL11, CBH91, Martin09]
+
+def main():
+
+    uKirja = Book("key1", "Kissan .byb", "Cook book", 2002, "WSOY")
+    reference = Reference(Martin09)
+
+
+if __name__ == "__main__":
+    main()
