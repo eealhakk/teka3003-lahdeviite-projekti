8e415c4888c72620103dd9814675a6d136b1077c
lahdelai <elisa.e.lahde@student.jyu.fi>
2025-11-22 14:41:41 +0200

    turhat pois

--- src/entities/viite.py
+++
@@ -1,5 +0,0 @@
-class Reference:
-    def __init__(self, type, key, fields=None):
-        self.type = type
-        self.key = key
-        self.fields = fields if fields else {}
--- src/services/viite_service.py
+++
@@ -1,5 +0,0 @@
-REFERENCE_TYPES = {
-    "inproceedings": ["author", "title","year", "booktitle"],
-    "article": ["author", "title", "journal", "year", "volume", "pages"],
-    "book": ["author", "title", "year", "publisher"]
-}
