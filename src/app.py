9dd278f87465ad76179f5aac0a2093d77100da85
roerleht <roerleht@jyu.fi>
2025-11-25 18:18:01 +0200

    pieni siivous appii

--- src/app.py
+++ src/app.py
@@ -109,10 +109,9 @@ class App:
 
         if type_choice == "1":
             return "inproceeding"
-        elif type_choice == "2":
+        if type_choice == "2":
             return "article"
-        elif type_choice == "3":
+        if type_choice == "3":
             return "book"
-        else:
-            self.io.write("Virheellinen valinta")
-            return None
+        self.io.write("Virheellinen valinta")
+        return None
