a6ddef0f1159d0c06457cd6834c671a12efcb432
lahdelai <elisa.e.lahde@student.jyu.fi>
2025-11-26 18:36:16 +0200

    databasemanager testi muutettu unittestiksi

--- src/tests/test_database_manager.py
+++ src/tests/test_database_manager.py
@@ -1,25 +1,26 @@
 """Testit DatabaseManager-luokan tietokantataulujen luonnille"""
 import sqlite3
-from repositories.viite_repository import DatabaseManager
+import unittest
+from repositories.database_manager import DatabaseManager
 
-def test_tables_created(tmp_path):
-    """Testaa, että DatabaseManager luo kaikki tarvittavat taulut"""
+class TestDatabaseManager(unittest.TestCase):
+    """Luokka jossa unittestit DatabaseManager-luokan funktioille"""
 
-    #Luodaan väliaikaisen kansion sisään uusi tyhjä tietokantatiedosto
-    db_file = tmp_path / "test.db"
+    def test_tables_created(self):
+        """Testaa, että DatabaseManager luo kaikki tarvittavat taulut"""
+        # Luodaan väliaikainen tietokantatiedosto
+        self.db_file = "test.db"
 
-    #Alustetaan DatabaseManager, joka luo taulut automaattisesti
-    DatabaseManager(db_name=str(db_file))
+        DatabaseManager(self.db_file)
 
-    #Avataan yhteys tietokantaan, jotta voidaan tarkistaa taulujen olemassaolo
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
+        conn = sqlite3.connect(self.db_file)
+        cur = conn.cursor()
 
-    #Haetaan kaikki tietokantaan luodut taulut sqlite_master-metataulusta
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    table_names = {row[0] for row in cur.fetchall()}
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names = {row[0] for row in cur.fetchall()}
+        conn.close()
 
-    #Varmistetaan, että kaikki odotetut taulut ovat olemassa
-    assert "inproceeding" in table_names
-    assert "article" in table_names
-    assert "book" in table_names
+        #Varmistetaan, että kaikki odotetut taulut ovat olemassa
+        assert "inproceeding" in table_names
+        assert "article" in table_names
+        assert "book" in table_names
