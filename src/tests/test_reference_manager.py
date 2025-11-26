090b264a963f5edf7e74fadcd6b6acd22d7fb96f
Ponttunen <mymememail2018@protonmail.com>
2025-11-26 17:18:57 +0200

    Refaktoroitu database ja viiterepoa seka refrens testejä.

--- src/tests/test_reference_manager.py
+++ src/tests/test_reference_manager.py
@@ -1,62 +1,66 @@
 """Testit ReferenceManager-luokalle"""
 import sqlite3
+import unittest
 from repositories.viite_repository import ReferenceManager
 
-def test_add_book(tmp_path):
-    """Testaa kirjan lisäämisen"""
-    # Luodaan väliaikainen tietokantatiedosto
-    db_file = tmp_path / "test.db"
+class TestReferenceManager(unittest.TestCase):
+    def setUp(self):
+        """Luodaan testisetuppi ReferenceManagerin testaamiseksi."""
+        # Luodaan väliaikainen tietokantatiedosto
+        self.db_file = "test.db"
 
-    # Luodaan ReferenceManager käyttäen tätä tietokantaa
-    ref = ReferenceManager()
-    ref.db_manager.db_name = str(db_file)  # vaihdetaan tietokanta testiin
-    ref.db_manager.create_database()       # luodaan taulut
+        # Luodaan ReferenceManager käyttäen tätä tietokantaa
+        self.ref = ReferenceManager(self.db_file)
 
-    # Lisättävä kirja
-    ref.add_book(
-        key="TEST1",
-        author="Test author",
-        title="Test title",
-        year=2020,
-        publisher="Test publisher"
-    )
 
-    # Tarkistetaan että kirja päätyi tietokantaan
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT key, author, title, year, publisher FROM book WHERE key='TEST1';")
-    result = cur.fetchone()
+        # Lisättävä kirja
+        self.ref.add_book(
+            key="TEST1",
+            author="Test author2",
+            title="Test title2",
+            year=2021,
+            publisher="Test publisher1"
+        )
 
-    assert result == ("TEST1", "Test author", "Test title", 2020, "Test publisher")
+        # Lisättävä artikkeli
+        self.ref.add_article(
+            key="TEST2",
+            author="Test author2",
+            title="Test title2",
+            journal="Test journal2",
+            year=2022,
+            volume="2",
+            pages="2"
+        )
 
-def test_add_article(tmp_path):
-    """Testaa artikkelin lisäämisen"""
-    # Luodaan väliaikainen tietokantatiedosto
-    db_file = tmp_path / "test.db"
+        # Lisättävä konferenssijulkaisu
+        self.ref.add_inproceeding(
+            key="TEST3",
+            author="Test author3",
+            title="Test title3",
+            year=2023,
+            booktitle="Test booktitle3"
+        )
 
-    # Luodaan ReferenceManager käyttäen tätä tietokantaa
-    ref = ReferenceManager()
-    ref.db_manager.db_name = str(db_file)  # vaihdetaan tietokanta testiin
-    ref.db_manager.create_database()       # luodaan taulut
 
-    # Lisättävä artikkeli
-    ref.add_article(
-        key="TEST1",
-        author="Test author",
-        title="Test title",
-        journal="Test journal",
-        year=2020,
-        volume="test",
-        pages="test"
-    )
+    def test_add_book(self):
+        # Tarkistetaan että kirja päätyi tietokantaan
+        conn = sqlite3.connect(self.db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT key, author, title, year, publisher FROM book WHERE key='TEST1';")
+        result = cur.fetchone()
 
-    # Tarkistetaan että artikkeli päätyi tietokantaan
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT key, author, title, journal, year, volume, pages "
-        "FROM article WHERE key='TEST1';"
-    )
-    result = cur.fetchone()
+        assert result == ("TEST1", "Test author", "Test title", 2020, "Test publisher")
 
-    assert result == ("TEST1", "Test author", "Test title", "Test journal", 2020, "test", "test")
+    def test_add_article(self):
+
+        # Tarkistetaan että artikkeli päätyi tietokantaan
+        conn = sqlite3.connect(self.db_file)
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT key, author, title, journal, year, volume, pages "
+            "FROM article WHERE key='TEST1';"
+        )
+        result = cur.fetchone()
+
+        assert result == ("TEST1", "Test author", "Test title", "Test journal", 2020, "test", "test")
