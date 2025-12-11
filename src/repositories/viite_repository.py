11b9b920a7b19a8e78ab2149520befccac5a6933
roerleht <roerleht@jyu.fi>
2025-12-11 17:15:30 +0200

    karvalakki filtteri

--- src/repositories/viite_repository.py
+++ src/repositories/viite_repository.py
@@ -42,17 +42,14 @@ class ReferenceManager:
 
     def filter_references(self, arvot):
         """Siistitään filter syötettä ja välitetään filtteröinti"""
-        try:
-            # Muutetaan syöte listaksi kokonaislukuja
-            choices = [int(x.strip()) for x in arvot.split(",")]
-        except ValueError:
-            print("Virheellinen syöte. Käytä numeroita pilkuilla eroteltuna.")
-            return "Virheellinen syöte. Käytä numeroita pilkuilla eroteltuna.\n"
-        print("=====================================")
-        #print (self.db_manager.filter_references_db(choices))
-        result = self.db_manager.filter_references_db(choices)
-        print(result)
-        return(str(self.db_manager.filter_references_db(choices)))
+
+        # Muutetaan syöte listaksi
+        choices = [x.strip() for x in arvot.split(",")]
+        print("choices testi:", choices)
+
+        print("===Tulokset:===")
+        rows = self.db_manager.filter_references_db(choices)
+        return rows
 
     def edit_entry(self, entry_type, target_key, **kwargs):
         """Muokkaa tietyn viitteen tietoja tietokannassa."""
